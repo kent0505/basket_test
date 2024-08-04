cfe3600f22c48c4e00b24644462a77c61231a24c
kent0505 <otabekyusupov0550@gmail.com>
2024-08-05 00:51:02 +0500

    added navbar

---
+++ lib/core/config/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color main = Color(0xffCB6100);
+  static const Color bg = Color(0xffF1F1F1);
+  static const Color navBarIndicator = Color(0xffFF7A00);
+
+  static Color card = const Color(0xff702626).withOpacity(0.5);
+  static Color white50 = const Color(0xffFFFFFF).withOpacity(0.5);
+}
